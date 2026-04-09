aec2eedb321b8d7fb4bf79507e48a341fe416332
Saksham <saksham.kj.3736@gmail.com>
2026-04-09 18:03:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, setDoc, doc, getDocs, collection, deleteDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 const layout = document.getElementById("layout");
 let selectedSeat = null;
 let assignments = JSON.parse(localStorage.getItem("seats")) || {};
